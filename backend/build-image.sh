764b81e841e30e58f1c96c1fcf36d244171971c5
Gabriel Cismoski <gabrielcismoski@gmail.com>
2025-07-03 20:47:57 -0300

    init

--- backend/build-image.sh
+++ backend/build-image.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-docker build -t gabrielcismoski/url-shortener-backend -f Dockerfile .
-docker image push gabrielcismoski/url-shortener-backend
-=======
 docker build -t cismoski/url-shortener-backend -f Dockerfile .
 docker image push cismoski/url-shortener-backend
->>>>>>> 1d164eb (init)
--- frontend/build-image.sh
+++ frontend/build-image.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-docker build -t gabrielcismoski/url-shortener-frontend -f Dockerfile .
-docker image push gabrielcismoski/url-shortener-frontend
-=======
 docker build -t cismoski/url-shortener-frontend -f Dockerfile .
 docker image push cismoski/url-shortener-frontend
->>>>>>> 1d164eb (init)
